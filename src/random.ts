063517ed9fd371e4411c0676c9f2ade0dc9097c3
Nolan Kovacik <noltron000@gmail.com>
2020-04-30 15:53:36 -0400

    [upd] improve types in `random.ts`

--- src/random.ts
+++ src/random.ts
@@ -1,19 +1,28 @@
-// which random function to use as default
+// which random function to use as default.
 const RANDOM = Math.random
 
-// random shuffle helper function
-// uses fisher yates randomizer
+// random shuffle helper function.
+// uses fisher yates randomizer.
 export const shuffle = (
-	array:Array<any>,
-	items:number = array.length,
-	random:Function = RANDOM,
-):Array<any> => {
-	const keys = [...array] // create copy
-	const results = [] // store choices here
-	for (let i=0; i<items; i++) {
-		const choice = Math.floor(random() * keys.length)
-		results.push(keys[choice]) // add choice to results
-		keys.splice(choice, 1) // remove choice from keys
+	list: Array<any> | Set<any>,
+	items?: number,
+	random: Function = RANDOM,
+): Array<any> => {
+
+	// create an array-type copy of input list.
+	const array: Array<any> = [...list]
+	// store final choices here.
+	const results: Array<any> = []
+
+	// retrieve a number of "items" equal to the given input.
+	for (let i = 0; i < items; i++) {
+
+		// choose a random index.
+		const choice = Math.floor(random() * array.length)
+		// add choice to results.
+		results.push(array[choice])
+		// remove choice from the array copy.
+		array.splice(choice, 1)
 	}
 	return results
 }
